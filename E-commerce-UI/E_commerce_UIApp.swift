9ca72aa35db0a4a004a81382d96cae4a58845759
BonalihengOun <bonalyhengun@gmail.com>
2024-08-27 19:10:12 +0700

    Initial Commit

---
+++ E-commerce-UI/E_commerce_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerce_UIApp.swift
+//  E-commerce-UI
+//
+//  Created by Ounbonaliheng on 27/8/24.
+//
+
+import SwiftUI
+
+@main
+struct E_commerce_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
