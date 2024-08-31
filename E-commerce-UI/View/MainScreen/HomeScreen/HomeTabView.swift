c3b082333668a45057c46e29443cb56b5a43ea75
BonalihengOun <bonalyhengun@gmail.com>
2024-08-31 14:59:51 +0700

    Favorite_Screen, SettingScreen,

---
+++ E-commerce-UI/View/MainScreen/HomeScreen/HomeTabView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTabView.swift
+//  E-commerce-UI
+//
+//  Created by Ounbonaliheng on 29/8/24.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeTabView()
+}
---
+++ E-commerce-UI/View/MainScreen/SettingScreen/ListSettingOptionScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ListSettingOptionScreen.swift
+//  E-commerce-UI
+//
+//  Created by Ounbonaliheng on 31/8/24.
+//
+
+import SwiftUI
+
+struct ListSettingOptionScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListSettingOptionScreen()
+}
